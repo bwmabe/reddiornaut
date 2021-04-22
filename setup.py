447b0d0285b923798b61c8a00154786c662a1fbb
bwmabe <9590854+bwmabe@users.noreply.github.com>
2021-04-22 16:49:58 -0400

    version number bump

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="reddiornaut",
-    version="0.0.2",
+    version="0.1.0",
     author="bwmabe",
     author_email="benwmabe@gmail.com",
     description="An async wrapper for reddit's JSON API",
